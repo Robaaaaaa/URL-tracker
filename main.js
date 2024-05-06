7bbdbc12b43feaa00998daabc3ab745b0a1152d5
Robaaa <linkmash030@gmail.com>
2024-05-06 16:12:21 +0300

    fixed minor bugs

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let myLeads = []
-let inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
@@ -33,23 +32,18 @@ inputBtn.addEventListener("click", function(){
     render(myLeads)
 })
 
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
 deleteBtn.addEventListener("dblclick",function(){
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-deleteBtn.addEventListener("click",function(){
-    myLeads.pop()
-    localStorage.setItem("myLeads",JSON.stringify( myLeads ))
-    render(myLeads)
-})
 
-tabBtn.addEventListener("click",function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(){
-        console.log(tabs)
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads",JSON.stringify( myLeads ))
-        render(myLeads)
-    })
-})
