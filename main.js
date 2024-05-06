32f8aeed3c2e7a1fc239786a614b7f67aebe6982
Robaaa <linkmash030@gmail.com>
2024-05-06 16:02:38 +0300

    Added a delete button and save current tab button

--- main.js
+++ main.js
@@ -2,25 +2,54 @@ let myLeads = []
 let inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl.value = ""
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for(let i = 0;i<myLeads.length;i++){
+    for(let i = 0;i<leads.length;i++){
         listItems += `
         <li>
-        <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
         </a>
         </li>
     `
     }
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("click",function(){
+    myLeads.pop()
+    localStorage.setItem("myLeads",JSON.stringify( myLeads ))
+    render(myLeads)
+})
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(){
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify( myLeads ))
+        render(myLeads)
+    })
+})
